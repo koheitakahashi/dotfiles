63f11cc5096a2d4a4c5fe154683afb7a133d24cd
koheitakahashi <426koohei@gmail.com>
2023-09-13 16:00:15 +0900

    univarsal変数でabbrを管理するのをやめてconfigファイルで管理するようにした
    https://qiita.com/nisshy82/items/bce027c4aa8372af7770 よりabbrの挙動が変わったため。

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,42 @@ set -gx FZF_LEGACY_KEYBINDINGS 0
 set -gx LESS -g -i -M -R -S -W -z-4 -x4
 set -gx GOPATH $HOME
 set -gx FZF_LEGACY_KEYBINDINGS 0
+
 # starshipの設定ファイルのパス
 set -gx STARSHIP_CONFIG ~/.starship/.config/starship.toml
 
-# iTerm2 の shell integratin
-source ~/.iterm2_shell_integration.fish
+# --------
+# abbr
+# --------
+abbr -a -- c code
+abbr -a -- cm 'code ./tmp/memo.md'
+abbr -a -- bu bundle
+abbr -a -- be 'bundle exec'
+abbr -a -- dkc 'docker compose'
+abbr -a -- dk docker
+abbr -a -- g git
+abbr -a -- gpro 'git branch --show-current | gh pr view --web'
+abbr -a -- gpl 'git pull'
+abbr -a -- ghv 'gh repo view -w' 
+abbr -a -- gst 'git stash'
+abbr -a -- glo 'git log --oneline'
+abbr -a -- gsw 'git switch'
+abbr -a -- gswc 'git switch -c'
+abbr -a -- gps 'git push'
+abbr -a -- gpsoh 'git push origin HEAD'
+abbr -a -- gpsfw 'git push --force-with-lease'
+abbr -a -- gop open_github_pr
+abbr -a -- less 'less -R'
+abbr -a -- opl 'open http://localhost:3000'
+abbr -a -- lg lazygit
+abbr -a -- cdic cd\ /Users/koheitakahashi/Library/Mobile\\\ Documents/com\~apple\~CloudDocs
+abbr -a -- e 'exa'
+abbr -a -- ef 'exa -F'
+abbr -a -- el 'exa -al'
+abbr -a -- b bat
+abbr -a -- r bin/rails
+abbr -a -- v vim
+abbr -a -- ws bin/webpack-dev-server
 
 # --------
 # function
@@ -32,3 +63,9 @@ end
 # --------
 eval "$(starship init fish)"
 eval (gh completion -s fish| source)
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# iTerm2 の shell integratin
+source ~/.iterm2_shell_integration.fish
+
